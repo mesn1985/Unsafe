ccdedaec86d544eca7409eef9445cb8a480a4510
Martin Edwin Schjødt Nielsen <MESN@ucl.dk>
2022-04-22 20:15:37 +0200

    Add project files.

---
+++ Unsafe.executionExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unsafe.executionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
